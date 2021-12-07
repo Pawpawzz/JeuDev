617c97305e333ca3c01958ab1e360e0b6e019186
Pawpawzz <lepioupioutueur@gmail.com>
2021-12-07 19:21:11 +0100

    Le Terrain

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,10 @@
 package main;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("bjr");
 
-	}
-
-	public static void lancerJeu() {
-		//
-		Terrain.GenererTerrain(2);
-	}
+//	public static void lancerJeu() {
+//		//
+//		Terrain.GenererTerrain(2);
+//	}
 
 }
--- src/main/Terrain.java
+++ src/main/Terrain.java
@@ -3,20 +3,43 @@ package main;
 public class Terrain {
 
     public static void placerObstacles(int minObstacles, int maxObstacles, int[][] terrain) {
-
+    	System.out.println(minObstacles + "" + maxObstacles);
+    	for (int i = 0; i < (int)(Math.random()*((maxObstacles - minObstacles) + 1) + minObstacles); i++) {
+    		terrain[(int) (Math.random()*terrain.length)][(int) (Math.random()*terrain.length)] = 1;
+    	}
     }
 
     public static void placerPieges(int minPieges, int maxPieges, int[][] terrain) {
-
+    	for (int i = 0; i < (int)(Math.random()*((maxPieges - minPieges) + 1) + minPieges); i++) {
+    		terrain[(int) (Math.random()*terrain.length)][(int) (Math.random()*terrain.length)] = 2;
+    	}
     }
 
-    public static int[][] GenererTerrain(int longueur) {
+    public static int[][] genererTerrain(int longueur) {
         int [][] nouveauTerrain = new int[longueur][longueur];
 
-
-        placerObstacles(1,2, nouveauTerrain);
-        placerPieges(1,2, nouveauTerrain);
+        
+        placerObstacles((int)(0.4*(double)longueur),(int)((double)longueur), nouveauTerrain);
+        placerPieges((int)(0.7*(double)longueur),(int)(1.2*(double)longueur), nouveauTerrain);
 
         return nouveauTerrain;
     }
+    
+    public static void afficherTerrain(int[][] terrain) {
+    	for (int i = 0; i < terrain.length; i++) {
+    		for (int j = 0; j < terrain.length; j++) {
+    			if (terrain[i][j] != 0)
+    				System.out.print(terrain[i][j]);
+    			else
+    				System.out.print(" ");
+    			System.out.print("|");
+    		}
+    		System.out.println();
+    	}
+    }
+    
+    public static void main(String[] args) {
+		afficherTerrain(genererTerrain(20));
+
+	}
 }
